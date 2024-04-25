17c417f5f2d500d3da57847c560d1239026e8d71
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-25 15:54:06 +0700

    Перегрузка методов. Практика 4 Урок 22, задача 12

---
+++ lesson_12.java
@@ -0,0 +1,19 @@
+public class lesson_12 {
+    //Напиши public static методы: int min(int, int), long min(long, long), double min(double, double).
+    //Каждый метод должен возвращать минимальное из двух переданных в него чисел.
+    public static void main(String[] args) {
+
+    }
+
+    public static int min(int a, int b) {
+        return Math.min(a, b);
+    }
+
+    public static long min(long a, long b) {
+        return Math.min(a, b);
+    }
+
+    public static double min(double a, double b) {
+        return Math.min(a, b);
+    }
+}
