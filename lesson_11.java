3a97c0d8fe5609a9cebec9d763d796c2a26482ad
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-25 15:53:28 +0700

    Перегрузка методов. Практика 3 Урок 22, задача 11

---
+++ lesson_11.java
@@ -0,0 +1,25 @@
+public class lesson_11 {
+    //Напиши пять методов print() с разными параметрами.
+    public static void main(String[] args) {
+    }
+
+    public static void print(int i) {
+
+    }
+
+    public static void print(double i) {
+
+    }
+
+    public static void print(short i) {
+
+    }
+
+    public static void print(byte i) {
+
+    }
+
+    public static void print(String i) {
+
+    }
+}
