96fb9ae1f62ab326d894072bb61571a2fa46fc29
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-25 15:52:31 +0700

    Перегрузка методов. Практика 2 Урок 22, задача 10

---
+++ lesson_10.java
@@ -0,0 +1,18 @@
+public class lesson_10 {
+    //Реализуй два метода: print(int) и print(Integer).
+    //Напиши такой код в методе main(), чтобы вызвались оба.
+    public static void main(String[] args) {
+        int i = 2;
+        print(i);
+        print(Integer.valueOf(i));
+    }
+
+    public static void print(int i)
+    {
+
+    }
+    public static void print(Integer i)
+    {
+
+    }
+}
