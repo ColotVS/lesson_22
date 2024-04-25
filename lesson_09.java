2b2f091b587038b7d6bde40b5699755e51880549
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-04-25 15:51:09 +0700

    Перегрузка методов. Практика 1 Урок 22, задача 9

---
+++ lesson_09.java
@@ -0,0 +1,15 @@
+public class lesson_09 {
+    //Напиши два метода: print(int) и print(String).
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int i)
+    {
+
+    }
+    public static void print(String s)
+    {
+
+    }
+}
