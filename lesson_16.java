609f5ca0ca70b08affe2c4fc45e56fd0e0c427ac
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-05-16 14:51:38 +0700

    Абстрактные классы. Практика 2 Урок 22, задача 16

---
+++ lesson_16.java
@@ -0,0 +1,14 @@
+public class lesson_16 {
+    //Исправь код, чтобы программа компилировалась.
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+
+        public abstract Pet getChild();
+    }
+}
