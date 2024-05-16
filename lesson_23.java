0d77f754564ed5ba48423c42a2ec54eb587aaf7a
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-05-16 15:35:29 +0700

    Интерфейсы. Практика 2 Урок 22, задача 23

---
+++ lesson_23.java
@@ -0,0 +1,19 @@
+public class lesson_23 {
+    //Напиши public интерфейсы CanFly(летать), CanRun(бежать/ездить), CanSwim(плавать).
+    //Добавить в каждый интерфейс по одному методу.
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void fly();
+    }
+
+    public interface CanRun {
+        void run();
+    }
+
+    public interface CanSwim {
+        void swim();
+    }
+}
