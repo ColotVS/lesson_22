a296119621c99088fc111bee299a07998eb7a9b4
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-05-16 15:33:56 +0700

    Интерфейсы. Практика 1 Урок 22, задача 22

---
+++ lesson_22.java
@@ -0,0 +1,12 @@
+public class lesson_22 {
+    //Напиши свой public интерфейс CanFly(летать).
+    //Добавь в него два метода.
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void iBelieve();
+        public void iCanFly();
+    }
+}
