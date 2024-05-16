49fea38a4cfd24666a83d0bb2f019972f3b94a39
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-05-16 14:53:44 +0700

    Абстрактные классы. Практика 4 Урок 22, задача 17

---
+++ lesson_18.java
@@ -0,0 +1,19 @@
+public class lesson_18 {
+    //Унаследуй класс Cow от Animal.
+    //Реализуй все недостающие методы в классе Cow.
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Animal {
+        public abstract String getName();
+    }
+
+    public static class Cow extends Animal{
+        @Override
+        public String getName()
+        {
+            return "Буренка";
+        }
+    }
+}
