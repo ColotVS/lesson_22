f43016b1e69e228b330b77aa1d368779408cbf64
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-05-17 12:11:32 +0700

    Бонусное задание Практика 13 Урок 22, задача 39

---
+++ lesson_39.java
@@ -0,0 +1,41 @@
+import static java.lang.Integer.MAX_VALUE;
+
+public class lesson_39 {
+    //Напиши метод, который возвращает минимальное число в массиве и его позицию (индекс).
+    public static void main(String[] args) throws Exception {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndIndex(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The index of the minimum element is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndIndex(int[] array) {
+        if (array == null || array.length == 0) {
+            return new Pair<>(null, null);
+        }
+
+        int min;
+        min = MAX_VALUE;
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (min > array[i]) {
+                min = array[i];
+                index = i;
+            }
+        }
+
+        return new Pair<>(min, index);
+    }
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
